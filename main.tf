60c38b696b5dc2de8f0f8cc41d1928b874cef8e1
sanjay0288 <87768165+sanjay0288@users.noreply.github.com>
2024-03-04 08:57:36 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"  
+  access_key = "AKIAVMY5CFMQFLPECWEH"
+  secret_key = "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"
+}
+module "ec2_instance" {
+  source = "./ec2_instance"
+  # Pass variables to the module
+  instance_name= "sas"
+  image_id = "ami-03f4878755434977f"
+  instance_type="t2.micro"
+  key_name="Sanjay"
+  security_group_ids=["sg-016d03c368df2f362"]
+  subnet_id="subnet-042fc13db89476ca4"
+
+}
